18501322d4f0210c6409f9cf7a52173fee4cb4e6
SaimirBunjaku <saimirbunjaku@gmail.com>
2025-06-21 06:39:24 +0200

    bg fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,8 +91,8 @@ export default {
 				'accordion-up': 'accordion-up 0.2s ease-out'
 			},
 			backgroundImage: {
-				'dot-pattern': "url(\"data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='10' cy='10' r='1' fill='rgba(255, 255, 255, 0.15)'/%3E%3C/svg%3E\")",
-				'dark-dot-pattern': "url(\"data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='10' cy='10' r='1' fill='rgba(255, 255, 255, 0.1)'/%3E%3C/svg%3E\")",
+				'dot-pattern': "radial-gradient(circle, rgba(255, 255, 255, 0.15) 1px, transparent 1px)",
+				'dark-dot-pattern': "radial-gradient(circle, rgba(255, 255, 255, 0.1) 1px, transparent 1px)",
 			},
 			backgroundSize: {
 				'dot-size': '20px 20px',
